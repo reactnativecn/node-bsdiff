12e2a547d7d5b0197d9447725da8de6d80c23b45
sunnylqm <sunnylqm@qq.com>
2026-06-03 15:36:39 +0800

    fix: load native prebuilds explicitly

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
  * Created by tdzl2003 on 2/28/16.
  */
 
-var native = require('node-gyp-build')(__dirname);
+function loadNative() {
+  var platform = process.platform;
+  var arch = process.arch;
+
+  if (platform === 'darwin' && arch === 'arm64') {
+    return require('./prebuilds/darwin-arm64/node-bsdiff.node');
+  }
+  if (platform === 'linux' && arch === 'x64') {
+    return require('./prebuilds/linux-x64/node-bsdiff.node');
+  }
+  if (platform === 'linux' && arch === 'arm64') {
+    return require('./prebuilds/linux-arm64/node-bsdiff.node');
+  }
+
+  return require('node-gyp-build')(__dirname);
+}
+
+var native = loadNative();
 exports.native = native;
 
 function byteLength(value) {
